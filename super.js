26bc366e19a8317a81e85e71beda1433887cf505
makhuga cindy <221181229@edu.vut.ac.za>
2025-03-05 11:16:14 +0200

    Implemented functionality to add, toggle, and remove todos with local storage persistence  - Modified the `addTodo` function to handle both new and pre-existing todos. - Checked if a `todo` object was passed, and if so, used its text and completed status to set the todo item appropriately. - Created a new `li` element for each todo and populated it with the todo text. - Added an event listener to toggle the "completed" class when the todo item is clicked, indicating the completion status. - Incorporated a `contextmenu` event listener to allow for right-click removal of a todo item from the list, while also updating the local storage. - Ensured that after adding or modifying a todo, the local storage is updated by calling `updateLS()` to persist the changes. - Cleared the input field after a todo was added to the list.
    enabling users to add their todos

--- super.js
+++ super.js
@@ -25,3 +25,58 @@ form.addEventListener('submit', (e) => {
 function addTodo(todo = null) {
 
     let todoText = input.value;
+	 if (todo) {
+
+        todoText = todo.text;
+
+    }
+
+    if (todoText) {
+        
+        const todoEl = document.createElement('li');
+
+
+        if (todo && todo.completed) {
+
+            todoEl.classList.add('completed');
+   
+        }
+
+   
+        todoEl.innerText = todoText;
+
+   
+        todoEl.addEventListener('click', () => {
+   
+            todoEl.classList.toggle('completed');
+   
+            updateLS();
+   
+        });
+
+        
+        todoEl.addEventListener('contextmenu', (e) => {
+        
+            e.preventDefault(); 
+       
+            todoEl.remove();
+        
+            updateLS();
+        
+        });
+
+
+        todosUl.appendChild(todoEl);
+
+        
+
+        input.value = '';
+
+
+        // Update local storage
+
+        updateLS();
+
+    }
+
+}
