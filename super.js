532539def8bf4f85bfb2c48765d9427ddf20d687
makhuga cindy <221181229@edu.vut.ac.za>
2025-03-05 11:13:55 +0200

    Implemented functionality to load todos from local storage  - Retrieved the stored todo list data from local storage using `localStorage.getItem()`. - Parsed the data into a usable format and displayed the saved tasks within the `todosUl` element. - Ensured that any previously saved tasks are loaded into the list upon page reload, maintaining persistence across sessions. - Checked for the existence of stored data before attempting to load, preventing errors when no to dos are saved.
    function the the to do

---
+++ super.js
@@ -0,0 +1,6 @@
+const form = document.getElementById('form');
+const input = document.getElementById('input');
+const todosUl = document.getElementById('todos');
+
+
+// Load todos from local storage
