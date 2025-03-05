a5674c1c8d365ae4df2f2c799dcfcfe70c5efa52
makhuga cindy <221181229@edu.vut.ac.za>
2025-03-05 11:14:53 +0200

    Added functionality to handle new todos and load from local storage  - Retrieved and parsed stored todos from local storage using `JSON.parse(localStorage.getItem('todos'))`. - If todos exist, iterated through each and called `addTodo` to render them on the page. - Added an event listener to the form to prevent the default form submission behavior and call `addTodo()` when the form is submitted. - Enhanced the `addTodo` function to accept an optional `todo` parameter, allowing both new todos and existing todos to be added to the list. - The `addTodo` function retrieves the input value and adds the new todo to the DOM, making it interactive and visible.
    making the visibility work

--- super.js
+++ super.js
@@ -4,3 +4,24 @@ const todosUl = document.getElementById('todos');
 
 
 // Load todos from local storage
+const todos = JSON.parse(localStorage.getItem('todos'));
+
+
+if (todos) {
+
+    todos.forEach(todo => addTodo(todo));
+
+}
+
+
+form.addEventListener('submit', (e) => {
+
+    e.preventDefault();  
+    
+    addTodo();  
+});
+
+
+function addTodo(todo = null) {
+
+    let todoText = input.value;
